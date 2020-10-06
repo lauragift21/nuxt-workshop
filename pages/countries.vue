bc2c4eed3b7e2867df197789bd2029cb072dcdec
Gift Egwuenu <laurandidi21@gmail.com>
2020-10-06 20:47:48 +0100

    add fetch implementation

---
+++ pages/countries.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="container mx-auto">
+    <h1 class="text-3xl text-center font-bold mb-5 underline">
+      Countries in the World Listing
+    </h1>
+    <p v-if="$fetchState.pending">Fetching countries...</p>
+    <p v-else-if="$fetchState.error">
+      Error while fetching posts: {{ $fetchState.error.message }}
+    </p>
+    <div v-else class="grid grid-cols-2">
+      <section
+        class="flex items-center mb-4"
+        v-for="country in countries"
+        :key="country.name"
+      >
+        <img :src="country.flag" :alt="country.name" class="w-1/2 h-1/2" />
+        <div class="px-4">
+          <span class="text-2xl font-bold">{{ country.name }}</span>
+          <span class="text-xl">{{ country.capital }}</span>
+          <p class="italic font-bold text-md">
+            Population: {{ country.population }}
+          </p>
+        </div>
+      </section>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      countries: [],
+    };
+  },
+  async fetch() {
+    this.countries = await this.$http.$get(
+      "https://restcountries.eu/rest/v2/all?fields=name;capital;flag;population"
+    );
+  },
+};
+</script>
