4ddb7128dc8efc1ea301474f5d33593a89688f41
Gift Egwuenu <laurandidi21@gmail.com>
2020-10-06 19:07:58 +0100

    update data fetching with async

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,16 +1,21 @@
 <template>
   <section class="container mx-auto">
-    <div>
+    <h2 class="text-4xl">Get Your GitHub Profile</h2>
+    <div class="my-4 flex items-center">
       <img
         :src="data.avatar_url"
         class="rounded-full w-1/4 mb-6"
         alt="profile image"
       />
-      <h2 class="text-2xl font-bold">{{ data.name }}</h2>
-      <span class="text-xl text-gray-700">{{ data.login }}</span>
-      <h2 class="pt-3">{{ data.bio }}</h2>
-      <span>Followers: {{ data.followers }}</span>
-      <span>Following: {{ data.following }}</span>
+      <div class="px-10">
+        <h2 class="text-3xl font-bold">{{ data.name }}</h2>
+        <p class="text-xl text-gray-900">{{ data.login }}</p>
+        <a class="pt-2 text-xl underline" :href="data.blog">{{ data.blog }}</a>
+        <h2 class="pt-2 text-xl">{{ data.bio }}</h2>
+        <h2 v-if="data.company" class="pt-2 text-xl font-bold">💼 {{ data.company }}</h2>
+        <span class="font-bold text-xl">Followers: {{ data.followers }}</span>
+        <span class="font-bold text-xl">Following: {{ data.following }}</span>
+      </div>
     </div>
   </section>
 </template>
