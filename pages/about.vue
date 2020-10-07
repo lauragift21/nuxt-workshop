6b247d2f9406a9073f9d586e6e398944e061d156
Gift Egwuenu <laurandidi21@gmail.com>
2020-10-07 14:24:58 +0100

    update exercise 2

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mx-64 my-4 my-12">
+  <section class="container ml-40 my-4 my-12">
     <h1 class="font-bold">About Me</h1>
     <h2 class="text-4xl">Hi there! I'm Gift Egwuenu</h2>
     <p class="text-2xl">I'm a {{ role }} at {{ work }}.</p>
